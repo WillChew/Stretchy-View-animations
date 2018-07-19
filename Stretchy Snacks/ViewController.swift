50f975ff8e052218302be872658a54c13f0597d2
WillChew <williamchew95@gmail.com>
2018-07-19 12:06:40 -0400

    Initial Commit

---
+++ Stretchy Snacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stretchy Snacks
+//
+//  Created by Will Chew on 2018-07-19.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
