af6a0d2e7db2ef97946baa4d4d3fe571edf6de9b
WillChew <williamchew95@gmail.com>
2018-07-19 12:59:42 -0400

    added image assets

--- Stretchy Snacks/ViewController.swift
+++ Stretchy Snacks/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusButtonOutlet: UIButton!
+    @IBOutlet weak var navBarHeight: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func plusButtonPressed(_ sender: UIButton) {
+        
+    expandNav()
+//        navBarHeight.constant = 200
+    }
+    
+    func expandNav(){
+        UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+            self.navBarHeight.constant = self.navBarHeight.constant == 64 ? 200 : 64
+            self.plusButtonOutlet.transform = self.plusButtonOutlet.transform.rotated(by: .pi/4)
+            self.view.layoutIfNeeded()
+        }) { finished in
+            print("plus button pressed")
+        }
+    }
+    
+    
+    
+    
+    
 }
 
